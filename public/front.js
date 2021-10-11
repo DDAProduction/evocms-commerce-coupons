33bbb48143fdea2d500f9f143382afe4ea92a309
dedli888 <dedli888@gmail.com>
2021-10-11 16:11:11 +0300

    rewrite front.js for native js

--- public/front.js
+++ public/front.js
@@ -1,26 +1,23 @@
-$(document).on('click','[data-commerce-coupon-add]',function (e) {
-    e.preventDefault();
-
-    let $errors = $('[data-commerce-coupon-errors]');
-
-    let coupon = $('[data-commerce-coupon]').val();
-
-    $.post('/commerce/coupons/add',{
-        coupon
-    },function (response) {
-
-
-        if(response.status){
-            $('[data-commerce-coupon-owner]').hide();
-            Commerce.updateOrderData($('[data-commerce-order]'));
-            $errors.hide();
-        }
-        else{
-            $errors.html(response.message);
-            $errors.show();
-        }
-
-
-    })
-
-});
+document.addEventListener('click', function (e) {
+    if (e.target.closest('[data-commerce-coupon-add]')) {
+        e.preventDefault();
+        let errors = document.querySelector('[data-commerce-coupon-errors]');
+        let coupon = document.querySelector('[data-commerce-coupon]').value;
+        const data = new URLSearchParams();
+        data.append('coupon', coupon);
+        fetch('/commerce/coupons/add', {method: 'post', body: data})
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                if (data.status) {
+                    document.querySelector('[data-commerce-coupon-owner]').style.display = 'none';
+                    Commerce.updateOrderData(document.querySelector('[data-commerce-order]'));
+                    errors.style.display = 'none';
+                } else {
+                    errors.innerHTML = data.message;
+                    errors.style.display = 'block';
+                }
+            });
+    }
+})
